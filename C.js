12638dd8c94b55c39274f0d09b7d0206fd81a3cb
LostX228 <abdekr40@gmail.com>
2024-09-16 03:50:02 +0100

    Add files via upload

---
+++ C.js
@@ -0,0 +1,15 @@
+
+function T1() {
+    location.href = "https://t.me/mrwanxios";
+    vant.showNotify({
+        type: 'success',
+        message: 'Grob Telegram'
+    });
+}
+function T2() {
+  location.href = "https://t.me/mrwanxchat";
+  vant.showNotify({
+        type: 'success',
+        message: 'Chat Telegram'
+    });
+}
